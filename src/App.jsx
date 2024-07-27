792c517e9cb354113fc6d90fb00689499885c2cb
ArameArakelyan <aramearakelyan.bit@gmail.com>
2024-07-27 13:59:06 +0400

    first component create

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+// 
+// import Form from './form';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Form></Form>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import './App.css';
+
+function App() {
+  const person = {
+    name :"Arame",
+    year : 2004
+  }
+  return (
+    <div className="text">
+      <p>My name is {person.name}. I born in {person.year}. I am {2024-person.year} years old</p>
+    </div>
+  )
+}
+
+export default App
