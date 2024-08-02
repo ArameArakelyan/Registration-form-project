666c6935e3876dc963d6294e809b803db820697a
ArameArakelyan <aramearakelyan.bit@gmail.com>
2024-08-02 22:25:13 +0400

    useStat

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+import { useState } from 'react';
+import { text } from './comp';
+import { maxLength } from './comp';
+
+function App() {
+  let [showText, setshowText] = useState(true)
+ function show() {
+    setshowText(!showText)
+}
+let [isDark, setIsDark] = useState(true)
+function makeDark(params) {
+  setIsDark(!isDark)
+}
+  return (
+    <div className="App" style={{background:isDark?"white":"black"}}>
+      <button className='btn' onClick={makeDark} >{isDark?"Dark":"Light"}</button>
+      <div className='textarea' style={{color:isDark?"black":"white"}}>
+        <h1>Information</h1>
+        <p>{showText?text.slice(0,maxLength):text}</p>
+        <span onClick={show} style={{color:"red", cursor:'pointer'}}>{showText?"More":"Less"}</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
