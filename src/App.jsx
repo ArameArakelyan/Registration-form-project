282523a188adb285aae316e7e90f96f912e657ba
ArameArakelyan <aramearakelyan.bit@gmail.com>
2024-08-03 12:05:03 +0400

    Less/More

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,9 @@ function makeDark(params) {
   setIsDark(!isDark)
 }
   return (
-    <div className="App" style={{background:isDark?"white":"black"}}>
+    <div className="App" style={{background:isDark?"white":"black", transition: "1s"}}>
       <button className='btn' onClick={makeDark} >{isDark?"Dark":"Light"}</button>
-      <div className='textarea' style={{color:isDark?"black":"white"}}>
+      <div className='textarea' style={{color:isDark?"black":"white",  transition: "1s"}}>
         <h1>Information</h1>
         <p>{showText?text.slice(0,maxLength):text}</p>
         <span onClick={show} style={{color:"red", cursor:'pointer'}}>{showText?"More":"Less"}</span>
